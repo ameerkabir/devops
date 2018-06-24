0226f47d36018ffa94c0aa79eb403a9d6e36ed45
Ameerkabir <ameerkabir2@gmail.com>
2018-06-24 17:50:56 +0100

    build file changed

--- deploy.sh
+++ deploy.sh
@@ -8,11 +8,3 @@ IMAGE="~DOCKER_USER/dockercmd"
 mkdir ~/.ssh
 echo "$PKEY" >> $KEY_FILE
 ssh $HOST -l $EC2_USER -i "$KEY_FILE" "sudo docker pull $IMAGE:$VERSION && sudo docker stop $CONTAINER && sudo docker rm $CONTAINER && sudo docker run --name $CONTAINER $IMAGE:$VERSION"
-workflows:
-  version: 2
-  build_and_deploy:
-    jobs:
-      - build 
-      - deploy:
-          requires:
-            - build
