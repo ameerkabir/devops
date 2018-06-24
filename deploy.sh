ba936e1f56264c7c4cb256dbb2b7f700a671161b
Ameerkabir <ameerkabir2@gmail.com>
2018-06-24 17:41:06 +0100

    added deployment

---
+++ deploy.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+HOST="34.243.242.180"
+EC2_USER="ec2-user"
+KEY_FILE="~/.ssh/devopstest.pem"
+
+mkdir ~/.ssh
+echo "$PKEY" >> $KEY_FILE
+ssh $HOST -l $EC2_USER -i "$KEY_FILE" "sudo docker pull $IMAGE:$VERSION && sudo docker stop $CONTAINER && sudo docker rm $CONTAINER && sudo docker run --name $CONTAINER $IMAGE:$VERSION"
+workflows:
+  version: 2
+  build_and_deploy:
+    jobs:
+      - build 
+      - deploy:
+          requires:
+            - build
--- index.js
+++ index.js
@@ -1,9 +1,13 @@
 const express = require('express')
 const app = express()
 const port = 8080
-
+const  human = {name: 'Ameer', age: 24, nationlity: 'Sudaness'}
     app.get('/', (req, res)=>{
         res.send('hello form docker')
+    })
+
+    app.get('/', (req, res)=>{
+      res.json(human)
     })
     app.listen(port,()=>{
         console.log('server is listen on '+  port)
